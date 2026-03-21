42f0fbeedce4105dab5bbdc455b6f92a15f6cc9d
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-03-21 16:10:45 +0900

    feat: update title of project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GeometricCanvas } from './components/GeometricCanvas';
 function App() {
   return (
     <div style={{ color: 'white', padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1 style={{ pointerEvents: 'none' }}>Geo Art Gen</h1>
+      <h1 style={{ pointerEvents: 'none' }}>Artora</h1>
       <p style={{ pointerEvents: 'none' }}>by ToraMutton</p>
 
       <GeometricCanvas />
