35be07c565566244cbb1c8e596d01e705480541e
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-03-21 19:07:57 +0900

    feat: prepare to add color changer

--- src/components/GeometricCanvas.tsx
+++ src/components/GeometricCanvas.tsx
@@ -29,6 +29,13 @@ const RESOLUTIONS = {
   'Android (1080×2400)': { w: 1080, h: 2400 },
 };
 
+const hexToRgb = (hex: string) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return `${r}, ${g}, ${b}`;
+};
+
 // ============================================
 
 export const GeometricCanvas = () => {
